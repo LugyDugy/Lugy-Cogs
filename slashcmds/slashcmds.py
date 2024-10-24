40044deab62267b6b0f160acc5363a47bcf242c2
LugyDugy <myusername.png@gmail.com>
2024-10-24 00:44:01 -0400

    reset

--- slashcmds/slashcmds.py
+++ slashcmds/slashcmds.py
@@ -1,11 +1,13 @@
-import discord
-
-from redbot.core import commands, app_commands
+from redbot.core import commands
 
 class MyCog(commands.Cog):
+    """My custom cog"""
+
     def __init__(self, bot):
         self.bot = bot
 
-    @app_commands.command()
-    async def hello(self, interaction: discord.Interaction):
-        await interaction.response.send_message("Hello World!", ephemeral=True)
+    @commands.command()
+    async def mycom(self, ctx):
+        """This does stuff!"""
+        # Your code will go here
+        await ctx.send("I can do stuff!")
