98a81ca93e42fcf5a9c5a43f54bc00a88b7c340c
Lugy <74808120+LugyDugy@users.noreply.github.com>
2025-06-24 12:20:34 -0400

    Update currencycon.py
    attempted fix pt 2

--- currencycon/currencycon.py
+++ currencycon/currencycon.py
@@ -12,7 +12,7 @@ class currencycon(commands.Cog):
     """
 
     __author__ = ["Lugy"]
-    __version__ = "1.1"
+    __version__ = "1.1.1"
 
     def __init__(self, bot: Red):
         self.bot = bot
@@ -30,7 +30,7 @@ class currencycon(commands.Cog):
         money2 = money2.upper()
 
         if len(money1) != 3:
-            await ctx.mresponse.send_message(
+            await ctx.response.send_message(
                 (
                     f"{money1} is formatted incorrectly"
                 )
@@ -50,11 +50,16 @@ class currencycon(commands.Cog):
               money1: str,
               money2: str,
     ):
-         convert = None
-         headers = {"User-Agent": f"Red Trusty-cogs conversions on {self.bot.user.name}"}
-         url = f"https://query1.finance.yahoo.com/v8/finance/chart/{money1}{money2}=x"
-         async with self.session.get(url, headers=headers) as query:
-            queryinfo = await query.json()
-         data = queryinfo.get("chart", {}).get("result", [])
-         convert = data[0].get("meta", {}).get("regularMarketPrice")
+        try:
+            convert = None
+            headers = {"User-Agent": f"Red Lugy-cogs currency conversion on {self.bot.user.name}", 
+                       "Accept": "application/json" 
+                      }
+            url = f"https://query1.finance.yahoo.com/v8/finance/chart/{money1}{money2}=x"
+            async with self.session.get(url, headers=headers) as query:
+                queryinfo = await query.json()
+            data = queryinfo.get("chart", {}).get("result", [])
+            convert = data[0].get("meta", {}).get("regularMarketPrice")
+        except Exception:
+            log.exception(f"Could not complete request")
          return convert
